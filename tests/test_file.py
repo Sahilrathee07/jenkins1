e2ab0bffa9e1114bf4e2887643bd5eda2c3a3290
Sahilrathee07 <sahilrathee07012001@gmail.com>
2024-05-06 13:16:59 +0530

    thord commit

--- tests/test_file.py
+++ tests/test_file.py
@@ -2,8 +2,8 @@ import unittest
 from src.calci import Calculator
 
 class TestCalculator(unittest.TestCase):
-    def setup(self):
-        self.calculator = Calculator
+    def setUp(self):
+        self.calculator = Calculator()
 
     def test_add(self):
         self.assertEqual(self.calculator.add(3, 5), 8)
